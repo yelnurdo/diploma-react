f3196b3ede4fbd592a7a4ff7dddfb463c8b27c29
yelnurdo <yelnurdos@gmail.com>
2024-06-13 00:53:03 +0500

    fix build

--- src/components/ReadingTestAnswerCard/index.tsx
+++ src/components/ReadingTestAnswerCard/index.tsx
@@ -1,5 +1,3 @@
-// src/components/ReadingTestAnswerCard/index.tsx
-
 import { FormEvent, useState } from "react";
 import classNames from "classnames";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -51,7 +49,7 @@ const ReadingTestAnswerCard: React.FC<Props> = ({ item }) => {
     event.preventDefault();
     try {
       setIsLoading(true);
-      await updateReadingTestAnswer(item.id, { ...item, feedback, ...answers });
+      await updateReadingTestAnswer(item.id, { ...answers, feedback });
       setIsLoading(false);
       alert('Feedback and answers updated successfully');
     } catch (error) {
@@ -147,7 +145,7 @@ const ReadingTestAnswerCard: React.FC<Props> = ({ item }) => {
               onChange={(e) => setFeedback(e.target.value)}
             />
           </div>
-          <Button text="Assess" onClick={handleUpdate} isLoading={isLoading} />
+          <Button text="Assess" onClick={() => handleUpdate} isLoading={isLoading} />
           </form>
       )}
     </div>
